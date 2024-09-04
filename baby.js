38693d6e26f20d4874ce6ce5b96fcf6970b96bbb
5rgtnhjigt4rho <55413668+9fkoafkoswfplspflpsld@users.noreply.github.com>
2024-09-04 19:44:12 -0400

    Update baby.js

--- baby.js
+++ baby.js
@@ -13,7 +13,7 @@ define("ace/theme/tomorrow_night", ["require", "exports", "module", "ace/lib/dom
     }
     
     .ace-tomorrow_night-theme {
-      background: url('https://m6.imhentai.xxx/020/4j60aqkz7f/221.jpg') no-repeat center center fixed;
+      background: url('https://merunyaa.xyz/wp-content/uploads/2021/12/STREAM-SUCC-4-wblur.png') no-repeat center center fixed;
       background-size: cover;
       color: #ffffff; 
       text-shadow: none;
